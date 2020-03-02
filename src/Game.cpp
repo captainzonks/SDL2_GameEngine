fd683b33726b2aa963f2b2df33854e1a0f96f2cb
captainzonks <zonks.zonks@gmail.com>
2020-03-01 18:17:15 -0700

    font and label component added

--- src/Game.cpp
+++ src/Game.cpp
@@ -7,7 +7,7 @@
 #include "./Components/SpriteComponent.h"
 #include "./Components/ColliderComponent.h"
 #include "./Components/KeyboardControlComponent.h"
-// #include "./Components/TextLabelComponent.h"
+#include "./Components/TextLabelComponent.h"
 #include "../lib/glm/glm.hpp"
 
 EntityManager manager;
@@ -36,6 +36,12 @@ void Game::Initialize(int width, int height)
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		std::cerr << "Error initializing SDL" << std::endl;
+		return;
+	}
+
+	if (TTF_Init() != 0)
+	{
+		std::cerr << "Error initializing SDL TTF" << std::endl;
 		return;
 	}
 
@@ -78,7 +84,7 @@ void Game::LoadLevel(int levelNumber)
 	assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
 	assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
 	assetManager->AddTexture("heliport-image", std::string("./assets/images/heliport.png").c_str());
-	// assetManager->AddFont("charriot-font", std::string("./assets/fonts/charriot.ttf").c_str(), 14);
+	assetManager->AddFont("charriot-font", std::string("./assets/fonts/charriot.ttf").c_str(), 14);
 
 	map = new Map("jungle-tiletexture", 2, 32);
 	map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
@@ -103,8 +109,8 @@ void Game::LoadLevel(int levelNumber)
 	radarEntity.AddComponent<TransformComponent>(720, 15, 0, 0, 64, 64, 1);
 	radarEntity.AddComponent<SpriteComponent>("radar-image", 8, 150, false, true);
 
-	//Entity& labelLevelName(manager.AddEntity("LabelLevelName", UI_LAYER));
-	//labelLevelName.AddComponent<TextLabelComponent>(10, 10, "First Level...", "charriot-font", WHITE_COLOR);
+	Entity& labelLevelName(manager.AddEntity("LabelLevelName", UI_LAYER));
+	labelLevelName.AddComponent<TextLabelComponent>(10, 10, "First Level...", "charriot-font", WHITE_COLOR);
 
 
 	// extra debug console outputs
